d60ac6654ab2ea77953eaeb1170b4fe9379e2305
Shuyuan <kukutou_mikiya@outlook.com>
2024-08-23 22:08:46 -0600

    add workexperience

--- src/portfolio.js
+++ src/portfolio.js
@@ -170,7 +170,7 @@ const techStack = {
 // Work experience section
 
 const workExperiences = {
-  display: false, //Set it to true to show workExperiences Section
+  display: true, //Set it to true to show workExperiences Section
   experience: [
     {
       role: "Software Engineer",
